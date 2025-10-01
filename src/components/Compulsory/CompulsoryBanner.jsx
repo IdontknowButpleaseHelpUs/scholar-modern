938fc50bf93031adec895bed1d0c4a2bcba0b607
Coconut <coconutistaster@gmail.com>
2025-10-02 03:12:12 +0700

    Update CompulsoryBanner.jsx

--- src/components/Compulsory/CompulsoryBanner.jsx
+++ src/components/Compulsory/CompulsoryBanner.jsx
@@ -1,12 +1,23 @@
 import styles from "../../styles/maincompo.module.css";
+import { useNavigate } from "react-router-dom";
 
 const CompulsoryBanner = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/');
+    };
+
     return (
-        <div className={styles.compulsory}>
+        <div 
+            className={styles.compulsory} 
+            onClick={handleClick}
+            style={{ cursor: 'pointer' }}
+        >
             <img src="../assets/se_kmitl_logo.png" alt="logo" className={styles.logo} />
             <p className={styles.compulsoryText}>
                 <span>S</span>oftware{" "}
-                <span style={{ color: "rgb(255,111,0)", textShadow: "2px 2px px black" }}>
+                <span style={{ color: "rgb(255,111,0)", textShadow: "2px 2px 5px black" }}>
                     E
                 </span>
                 ngineering
